1554c3879b39cb5db24935188c9dd0ebbbfce5f8
nsnail <taokeu@gmail.com>
2012-04-10 22:57:59 +0800

    optimize cpu&mem utilization

--- src/spidernet/prog.cs
+++ src/spidernet/prog.cs
@@ -26,8 +26,10 @@
 // THE SOFTWARE.
 #endregion
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -37,7 +39,6 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using CommandLine;
 using CommandLine.Text;
-using System.Diagnostics;
 using log4net;
 
 namespace spidernet
@@ -49,10 +50,8 @@ namespace spidernet
 		/// </summary>
 		private static readonly ILog _log = LogManager.GetLogger(typeof(prog));
 
-		/// <summary>
-		/// 抓取数据存储文件是否创建的标示.
-		/// </summary>
-		private static bool _db_created;
+		private static db_mgr _dbm;
+
 		/// <summary>
 		/// 版本描述
 		/// </summary>
@@ -68,47 +67,47 @@ namespace spidernet
 		/// url树
 		/// </summary>
 		private static node _root_node;
+
+		/// <summary>
+		/// 线程消息
+		/// </summary>
 		private static Dictionary<Thread, string> _thread_msg_dic;
+
+		/// <summary>
+		/// 已访问的url
+		/// </summary>
+		private static readonly Dictionary<int, object> _visited_url = new Dictionary<int, object>();
+
 		/// <summary>
 		/// 程序统计信息
 		/// </summary>
 		private static prog_cter prog_stat;
+
 		/// <summary>
-		/// 获取资源, 构建和遍历和url树.
+		/// 获取资源的纯文本格式.
 		/// </summary>
-		/// <param name="start_node">起始节点</param>
-		private static void crawl(ref node start_node)
+		/// <param name="req_url"></param>
+		/// <returns></returns>
+		private static string get_res_text(string req_url)
 		{
-			if (start_node.depth > _opts.crawl_depth) return; //爬行深度超过限制, 退出.
-
-			//锁定node
-			lock (start_node)
-			{
-				if (start_node.locked)
-					return;
-				start_node.locked = true;
-			}
-			lock (_log)
-				_log.InfoFormat("requesting\t{0}", start_node.url);
-
 			//资源解码后存储的str
-			string html = null;
+			string res = null;
 			try
 			{
-				HttpWebRequest req = WebRequest.Create(start_node.url) as HttpWebRequest;
+				HttpWebRequest req = WebRequest.Create(req_url) as HttpWebRequest;
 				req.Timeout = _opts.timeout;
 				req.AllowAutoRedirect = false; //TODO: 避免死递归, 禁止301等重定向.(需改进)
 				byte[] down_bytes = new byte[_opts.bytes_max];
 				byte[] down_buf = new byte[down_bytes.Length / 10]; //缓存区, 1/10的容器大小.
-				int down_bytes_cursor = 0;//容器填充位置
+				int down_bytes_cursor = 0; //容器填充位置
 				using (WebResponse rep = req.GetResponse())
 				{
 					++prog_stat.res_downloaded;
 					if (rep.Headers[HttpResponseHeader.ContentType] == null
 						|| !rep.Headers[HttpResponseHeader.ContentType].Contains("text/html"))
 					{
-						start_node.valid = false;//过滤非text/html资源类型
-						return;
+						//过滤非text/html资源类型
+						return null;
 					}
 					using (Stream sr = rep.GetResponseStream())
 					{
@@ -130,7 +129,7 @@ namespace spidernet
 						{
 							byte[] decode_bytes = new byte[down_bytes.Length];
 							byte[] decode_buf = new byte[down_bytes.Length / 10];
-							int decode_bytes_cursor = 0;//容器填充位置
+							int decode_bytes_cursor = 0; //容器填充位置
 							int read_size;
 							while ((read_size = gs.Read(decode_buf, 0, decode_buf.Length)) > 0)
 							{
@@ -149,30 +148,29 @@ namespace spidernet
 					if (rep.Headers[HttpResponseHeader.ContentType] != null)
 					{
 						if (rep.Headers[HttpResponseHeader.ContentType].Contains("utf-8"))
-							html = Encoding.UTF8.GetString(down_bytes, 0, down_bytes_cursor);
+							res = Encoding.UTF8.GetString(down_bytes, 0, down_bytes_cursor);
 						else if (rep.Headers[HttpResponseHeader.ContentType].Contains("gb2312")
-							|| rep.Headers[HttpResponseHeader.ContentType].Contains("gbk"))
-							html = Encoding.GetEncoding(54936).GetString(down_bytes, 0, down_bytes_cursor);
+								 || rep.Headers[HttpResponseHeader.ContentType].Contains("gbk"))
+							res = Encoding.GetEncoding(54936).GetString(down_bytes, 0, down_bytes_cursor);
 					}
 				}
 
-				if (html == null)
+				if (res == null)
 				{
 					//http head中未指定encoding的资源, 暂以utf8统一解码.
-					html = Encoding.UTF8.GetString(down_bytes, 0, down_bytes_cursor);
+					res = Encoding.UTF8.GetString(down_bytes, 0, down_bytes_cursor);
 
-					Match m = _regex_charset.Match(html);//寻找<meta>标记中的charset
+					Match m = _regex_charset.Match(res); //寻找<meta>标记中的charset
 					if (!m.Success)
 					{
-						//http body中也没有指定encoding, 标记资源无效.
-						start_node.valid = false;
-						return;
+						//http body中也没有指定encoding, 资源无效.
+						return null;
 					}
 
 					//http body中指定了gbk编码
 					if (m.Groups[1].Value.IndexOf("gb2312", StringComparison.OrdinalIgnoreCase) >= 0
 						|| m.Groups[1].Value.IndexOf("gbk", StringComparison.OrdinalIgnoreCase) >= 0)
-						html = Encoding.GetEncoding(54936).GetString(down_bytes, 0, down_bytes_cursor);
+						res = Encoding.GetEncoding(54936).GetString(down_bytes, 0, down_bytes_cursor);
 
 				}
 			}
@@ -180,88 +178,73 @@ namespace spidernet
 			{
 				//TODO: 记录异常日志.
 			}
+			return res;
+		}
 
-			if (html == null)
+		/// <summary>
+		/// 获取资源, 构建和遍历和url树.
+		/// </summary>
+		/// <param name="start_node">起始节点</param>
+		private static void crawl(ref node start_node)
+		{
+			//锁定node
+			lock (start_node)
 			{
-				//str容器未得到设置, 标记node无效.
-				start_node.valid = false;
-				return;
+				if (start_node.locked)
+					return;
+				start_node.locked = true;
 			}
+			lock (_log)
+				_log.InfoFormat("requesting\t{0}", start_node.url);
 
 			{
+
+				string res = get_res_text(start_node.url);
+
+				if (res == null)
+				{
+					//str容器未得到设置, 标记node无效.
+					start_node.valid = false;
+					return;
+				}
+
+				string title = null;
+				Match m = _regex_title.Match(res); //获取html中<title>内容
+				if (m.Success)
+					title = m.Groups[1].Value.Replace("\n", "").Replace("\r", "").Trim();
+				//写入数据库
+				_dbm.write_to_db_cache(start_node.url, title, res, DateTime.Now);
+				++prog_stat.res_stored;
+
+				//当前爬行深度已达到限制, 不再增加child.
+				if (start_node.depth >= _opts.crawl_depth) return;
+
 				//匹配html中的href,将这些url作为自己的children集合.
-				MatchCollection mc = _regex_href.Matches(html);
+				MatchCollection mc = _regex_href.Matches(res);
 				IEnumerable<string> mc_str =
 					(mc.AsParallel() as IEnumerable<Object>).Select(f => (f as Match).Groups[1].Value).Distinct();
 				foreach (string href in mc_str)
 				{
 					string child_url = filter.filter_href(href, start_node.url);
 					if (child_url == null) continue;
-					node child = new node { url = child_url, parent = start_node, depth = start_node.depth + 1 };//深度+1
-					if (isdup(child_url, _root_node)) continue;//url重复检查.
+					node child = new node { url = child_url, parent = start_node, depth = start_node.depth + 1 }; //深度+1
+					if (isdup(child_url)) continue; //url重复检查.
+					_visited_url.Add(child_url.GetHashCode(), null);
 					lock (start_node)
-						start_node.children.Add(child);//添加children,这些新的children是未lock状态, 会马上被空闲的线程抢到并遍历.
+						start_node.children.Add(child); //添加children,这些新的children是未lock状态, 会马上被空闲的线程抢到并遍历.
 				}
-			}
+			}//释放res
 
 			//遍历child
 			for (int i = 0; i != start_node.children.Count; ++i)
 			{
 				node child = start_node.children[i];
 				lock (_thread_msg_dic)
-					_thread_msg_dic[Thread.CurrentThread] = child.url;
+					_thread_msg_dic[Thread.CurrentThread] = "d" + child.depth + "::" + child.url;
 				crawl(ref child);
 			}
-
-			if (!start_node.valid) return; //无效节点, 不写入数据库.
-
-			//写入数据库
-			if (!_db_created)
-			{
-				create_db(_opts.db_path);
-				_db_created = true;
-			}
-			using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + _opts.db_path))
-			using (SQLiteCommand cmd = new SQLiteCommand(conn))
-			{
-
-				conn.Open();
-				cmd.CommandText = "insert into crawl values(@url,@title,@html,@createtime)";
-
-				string title = null;
-				Match m = _regex_title.Match(html);//获取html中<title>内容
-				if (m.Success)
-					title = m.Groups[1].Value.Replace("\n", "").Replace("\r", "").Trim();
-
-				cmd.Parameters.AddRange(new[] {
-			        new SQLiteParameter("@url", start_node.url),
-			        new SQLiteParameter("@title", title),
-			        new SQLiteParameter("@html", html),
-                    new SQLiteParameter("@createtime", DateTime.Now)
-				                              });
-				cmd.ExecuteNonQuery();
-				++prog_stat.res_stored;
-			}
 		}
-		/// <summary>
-		/// 创建sqlite数据库, 和表.
-		/// </summary>
-		/// <param name="db_path">数据库文件路径</param>
-		private static void create_db(string db_path)
-		{
-			using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + db_path))
-			using (SQLiteCommand cmd = new SQLiteCommand(conn))
-			{
-				conn.Open();
-				cmd.CommandText = @"CREATE TABLE [crawl] (
-  [url] NVARCHAR, 
-  [title] NVARCHAR, 
-  [html] NTEXT, 
-  [createtime] DATETIME, 
-  CONSTRAINT [sqlite_autoindex_crawl_1] PRIMARY KEY ([url]) ON CONFLICT IGNORE);";
-				cmd.ExecuteNonQuery();
-			}
-		}
+
 		/// <summary>
 		/// 获取未锁定的url节点.
 		/// </summary>
@@ -277,24 +260,17 @@ namespace spidernet
 						: root.children.Select(find_nolock_node).FirstOrDefault(f => f != null);
 			}
 		}
+
 		/// <summary>
-		/// 检查指定url是否在整棵树中存在, 避免死循环.
+		/// url重复check
 		/// </summary>
-		/// <param name="url">检查url</param>
-		/// <param name="root">树根</param>
-		/// <returns>存在重复返回true</returns>
-		private static bool isdup(string url, node root)
+		/// <param name="url"></param>
+		/// <returns></returns>
+		private static bool isdup(string url)
 		{
-			lock (root)
-			{
-				foreach (node child in root.children)
-				{
-					if (child.url == url) return true;
-					if (isdup(url, child)) return true;
-				}
-				return false;
-			}
+			return _visited_url.ContainsKey(url.GetHashCode());
 		}
+
 		private static void Main(string[] args)
 		{
 			//解析命令行参数
@@ -304,7 +280,7 @@ namespace spidernet
 				Environment.Exit(1);
 
 			_root_node = new node { url = _opts.url_start };
-			_db_created = File.Exists(_opts.db_path);
+			_dbm = new db_mgr(_opts.db_path);
 
 			//启动工作线程
 			_thread_msg_dic = new Dictionary<Thread, string>(_opts.thread_cnt);
@@ -324,7 +300,7 @@ namespace spidernet
 				Console.WriteLine();
 				Process proc_self = Process.GetCurrentProcess();
 				Console.WriteLine("working time: {0}", (DateTime.Now - proc_self.StartTime));
-				Console.WriteLine("mem used: {0} KB", (proc_self.WorkingSet64 / 1024).ToString("###,###"));
+				Console.WriteLine("mem usage: {0} KB", (proc_self.WorkingSet64 / 1024).ToString("###,###"));
 				Console.WriteLine("res downloaded:{0}\tres stored:{1}", prog_stat.res_downloaded, prog_stat.res_stored);
 				Console.WriteLine("threads:");
 				lock (_thread_msg_dic)
